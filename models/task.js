d63b06a9de51fc2af3e05f525a18f09a31bac3ce
Pearl <pearl.rathour33@gmail.com>
2023-07-27 21:26:32 +0530

    Added TO DO APi

--- models/task.js
+++ models/task.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Pearl@1973",
+    password: "Enter your password",
     database: "todo_db"
 })
 
